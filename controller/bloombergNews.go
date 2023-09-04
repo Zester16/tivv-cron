ae52eea5d21e863319ed44b70397f3bbc3dadd3f
mharsh15 <zesterschmid16@gmail.com>
2023-09-04 22:26:46 +0530

    feat/tivv-v_0_0_4: add method

--- controller/bloombergNews.go
+++ controller/bloombergNews.go
@@ -7,14 +7,24 @@ import (
 )
 
 func GetBloombergTechNews(w http.ResponseWriter, r *http.Request) {
-	//bloomberg := "https://www.bloomberg.com/newsletters/five-things-europe/lates"
-	body, err := http.Get("https://m.com")
+	client := &http.Client{}
+
+	bloomberg := "https://www.bloomberg.com/newsletters/five-things-europe/latest"
+
+	req, _ := http.NewRequest("GET", bloomberg, nil)
+	req.Header = http.Header{"Cookie": {"table: 0x7f0fe1d04bf8; exp_pref=AMER; seen_uk=1; country_code=US"}, "Host": {"www.bloomberg.com"}, "UserAgent": {"PostmanRuntime/7.32.3"}}
+	//req.Header.Set()
+
+	rep, _ := client.Do(req)
+
+	//body, err := http.Get(bloomberg)
+
+	fmt.Print(rep.StatusCode)
+	response, err := ioutil.ReadAll(rep.Body)
 	if err != nil {
 		//log.Fatal(err)
 		//w.Write([]byte("Not our page"))
 	}
-	fmt.Print(body.StatusCode)
-	response, err := ioutil.ReadAll(body.Body)
 	w.Write(response)
 
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.21.0
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
