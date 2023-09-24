1b8d4d1f735f2179d5f8fbf977d5b882a7d75dbf
mharsh15 <zesterschmid16@gmail.com>
2023-09-24 14:44:00 +0530

    feat/tivv-v_0_0_10: update cron job function to do check date and update. 30 limit array requirement needs to be done

--- cron/bqPrimeCron.go
+++ cron/bqPrimeCron.go
@@ -3,8 +3,11 @@ package cron
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"stockpull/datasource"
 	"stockpull/utils"
+	"strconv"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -24,14 +27,27 @@ func SetBqPrimeNEwsLetterArray() {
 
 	url := utils.GetBQPrimeUrl()
 
-	var bqArray = []NewsObject{{Date: "23-09-2023", NewsUrl: url}}
+	tm := time.Now()
+	month := tm.Month().String()
+	day := strconv.Itoa(tm.Day())
+	yr := strconv.Itoa(tm.Year())
+	todaysDate := day + "-" + month + "-" + yr
+
+	var bqArray = []NewsObject{{Date: "2" + todaysDate, NewsUrl: url}}
 
 	if err != redis.Nil {
 		newsObject := []NewsObject{}
-		err := json.Unmarshal([]byte(bqArrayString), &newsObject)
-		bqArray = append(bqArray, newsObject...)
-		if err != nil {
+		errJsn := json.Unmarshal([]byte(bqArrayString), &newsObject)
+
+		if bqArray[0].Date != newsObject[0].Date {
 			bqArray = append(bqArray, newsObject...)
+		} else {
+			bqArray = newsObject
+		}
+
+		fmt.Println(newsObject[0].Date)
+		if errJsn != nil {
+			fmt.Printf(errJsn.Error())
 		}
 
 	}
