5e50aac2c58a8af22d791a97bf1c16c30c6ff5f3
mharsh15 <zesterschmid16@gmail.com>
2024-03-31 22:08:19 +0530

    chore/tivv-v_0_0_57: add cached forex endpoint

--- controller/forex.go
+++ controller/forex.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"encoding/json"
 	"net/http"
+	"stockpull/datasource"
+	"stockpull/model"
 	"stockpull/network"
 )
 
@@ -13,5 +15,16 @@ func GetCurrencyValue(w http.ResponseWriter, r *http.Request) {
 	j, _ := json.Marshal(resp)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(j))
+
+}
+
+func GetCachedCurrencyValue(w http.ResponseWriter, r *http.Request) {
+
+	rdx := datasource.RedisConnect()
+
+	resp, _ := rdx.RedisDBConnector.Get(ctx, model.FOREX_KEY_NAME).Result()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(resp))
 
 }
