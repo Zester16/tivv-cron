73937e79de592f9c488a6610ff20eef3568d9d39
mharsh15 <zesterschmid16@gmail.com>
2024-03-31 07:32:54 +0530

    chore/tivv-v_0_0_53: add error handling utils

---
+++ utils/errorHandler.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type errorMessage struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"statusCode"`
+}
+
+func SetErrorForNoQuery(w http.ResponseWriter) {
+	sendResponse400(w, errorMessage{Message: "no query passed", StatusCode: 1})
+}
+
+func SetErrorForNoMatchingQuery(w http.ResponseWriter) {
+	sendResponse400(w, errorMessage{Message: "Wrong find", StatusCode: 2})
+}
+
+func sendResponse400(w http.ResponseWriter, em errorMessage) {
+	str, _ := json.Marshal(em)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+	w.Write(str)
+}
