e2c3ac5a72718fe3f7a7bf2cda7b1e0eee9e92fe
mharsh15 <zesterschmid16@gmail.com>
2025-08-05 10:01:41 +0530

    chore/tivv-v_0_0_65: update bqprime url and accompanying logic

--- utils/bqPrimeNewsLetter.go
+++ utils/bqPrimeNewsLetter.go
@@ -15,7 +15,9 @@ func GetBQPrimeUrl() string {
 	baseString := `https://www.ndtvprofit.com/business/stock-market-today-all-you-need-to-know-going-into-trade-on-`
 
 	//till june 2025: works in 2025 for certain days
-	baseString_2024 := `https://www.ndtvprofit.com/markets/stock-market-today-all-you-need-to-know-before-going-into-trade-on-`
+	//baseString_2024 := `https://www.ndtvprofit.com/markets/stock-market-today-all-you-need-to-know-before-going-into-trade-on-`
+
+	baseString_2025 := `https://www.ndtvprofit.com/markets/stock-market-today-all-you-need-to-know-going-into-trade-on`
 
 	tm := time.Now()
 	month := tm.Month().String()
@@ -23,43 +25,44 @@ func GetBQPrimeUrl() string {
 	date := tm.Day()
 	yr := YearToEdgePasser(tm.Year())
 	yearAsString := strconv.Itoa(tm.Year())
-	// monthShortner := getMonthAsPerCurrentMonth(month) // stubbing it 2025 aug since short month may not be in use
+	monthShortner := getMonthAsPerCurrentMonth(month) // stubbing it 2025 aug since short month may not be in use
 
 	//base string
 	fullString := baseString + monthLowerCase + "-" + strconv.Itoa(date)
-	fullString2024 := baseString_2024 + month + "-" + strconv.Itoa(date)
+	fullString2025 := baseString_2025 + "-" + monthShortner + "-" + strconv.Itoa(date)
 
 	//creating url
-	fullStringUrl2024 := fullString2024 + "-" + strconv.Itoa(yr) //example: https://www.ndtvprofit.com/markets/stock-market-today-all-you-need-to-know-going-into-trade-on-july-23-2
-	oneDecrementUrl2024 := fullString2024 + strconv.Itoa(yr-1)
+	fullStringUrl2025 := fullString2025 + "-" + strconv.Itoa(yr) //example: https://www.ndtvprofit.com/markets/stock-market-today-all-you-need-to-know-going-into-trade-on-july-23-2
+	oneDecrementUrl2025 := fullString2025 + "-" + strconv.Itoa(yr-1)
 
 	//old url decreament
 	fullStringUrl := fullString + "-" + yearAsString //https://www.ndtvprofit.com/markets/stock-market-today-all-you-need-to-know-going-into-trade-on-july-30-2025
 	//oneDecrementUrl := fullString + "-" + strconv.Itoa(yr-1)
 
-	fmt.Println("Utils:GetBQPrime:fullstring2024: ", fullString2024)
+	fmt.Println("Utils:GetBQPrime:fullstringUrl2025: ", fullStringUrl2025)
+	fmt.Println("Utils:GetBQPrime:oneDecrementUrl2025: ", oneDecrementUrl2025)
 	fmt.Println("Utils:GetBQPrime:fullstring: ", fullString)
 	fmt.Println("Utils:GetBQPrime:fullstringURL: ", fullStringUrl)
 
 	req, _ := http.Get(fullStringUrl)
 
 	if req.StatusCode == 200 {
-		fmt.Printf("in full string url")
+		fmt.Printf("in full string url", req.StatusCode)
 		return fullStringUrl
 	}
 
-	req2024, _ := http.Get(fullStringUrl2024)
+	req2024, _ := http.Get(fullStringUrl2025)
 
 	if req2024.StatusCode == 200 {
 		fmt.Printf("in 2024 full string url")
-		return fullStringUrl2024
+		return fullStringUrl2025
 	}
 
-	req2024Decr, _ := http.Get(oneDecrementUrl2024)
+	req2024Decr, _ := http.Get(oneDecrementUrl2025)
 
 	if req2024Decr.StatusCode == 200 {
-		fmt.Println("in 2024 one string decreament ")
-		return oneDecrementUrl2024
+		fmt.Println("in 2025 one string decreament ")
+		return oneDecrementUrl2025
 	}
 
 	//commenting this code as of 2025-08-03  since it not required
